360c3172387aea18f53a4902d2e89f91e70cc692
Frans van Buul <frans.vanbuul@axoniq.io>
2018-07-04 22:35:03 +0200

    added comments

--- src/main/java/com/example/giftcard/gui/CardSummaryDataProvider.java
+++ src/main/java/com/example/giftcard/gui/CardSummaryDataProvider.java
@@ -29,8 +29,8 @@ public class CardSummaryDataProvider extends AbstractBackEndDataProvider<CardSum
      * we are processing query updates, the methods on these class are synchronized.
      */
 
-    private SubscriptionQueryResult<List<CardSummary>, CardSummary> fetchQuery;
-    private SubscriptionQueryResult<Integer, CountChangedUpdate> countQuery;
+    private SubscriptionQueryResult<List<CardSummary>, CardSummary> fetchQueryResult;
+    private SubscriptionQueryResult<Integer, CountChangedUpdate> countQueryResult;
 
     @Getter
     @Setter
@@ -40,34 +40,51 @@ public class CardSummaryDataProvider extends AbstractBackEndDataProvider<CardSum
     @Override
     @Synchronized
     protected Stream<CardSummary> fetchFromBackEnd(Query<CardSummary, Void> query) {
-        if (fetchQuery != null) {
-            fetchQuery.cancel();
-            fetchQuery = null;
+        /*
+         * If we are already doing a query (and are subscribed to it), cancel are subscription
+         * and forget about the query.
+         */
+        if (fetchQueryResult != null) {
+            fetchQueryResult.cancel();
+            fetchQueryResult = null;
         }
         FetchCardSummariesQuery fetchCardSummariesQuery =
                 new FetchCardSummariesQuery(query.getOffset(), query.getLimit(), filter);
         log.trace("submitting {}", fetchCardSummariesQuery);
-        fetchQuery = queryGateway.subscriptionQuery(fetchCardSummariesQuery,
+        /*
+         * Submitting our query as a subscriptionquery, specifying both the initially expected
+         * response type (multiple CardSummaries) as wel as the expected type of the updates
+         * (single CardSummary object). The result is a SubscriptionQueryResult which contains
+         * a project reactor Mono for the initial response, and a Flux for the updates.
+         */
+        fetchQueryResult = queryGateway.subscriptionQuery(fetchCardSummariesQuery,
                 ResponseTypes.multipleInstancesOf(CardSummary.class),
                 ResponseTypes.instanceOf(CardSummary.class));
-        fetchQuery.updates().subscribe(
+        /*
+         * Subscribing to the updates before we get the initial results.
+         */
+        fetchQueryResult.updates().subscribe(
                 cardSummary -> {
                     log.trace("processing query update for {}: {}", fetchCardSummariesQuery, cardSummary);
+                    /* This is a Vaadin-specific call to update the UI as a result of data changes. */
                     fireEvent(new DataChangeEvent.DataRefreshEvent(this, cardSummary));
                 });
-        return fetchQuery.initialResult().block().stream();
+        /*
+         * Returning the initial result.
+         */
+        return fetchQueryResult.initialResult().block().stream();
     }
 
     @Override
     @Synchronized
     protected int sizeInBackEnd(Query<CardSummary, Void> query) {
-        if (countQuery != null) {
-            countQuery.cancel();
-            countQuery = null;
+        if (countQueryResult != null) {
+            countQueryResult.cancel();
+            countQueryResult = null;
         }
         CountCardSummariesQuery countCardSummariesQuery = new CountCardSummariesQuery(filter);
         log.trace("submitting {}", countCardSummariesQuery);
-        countQuery = queryGateway.subscriptionQuery(countCardSummariesQuery,
+        countQueryResult = queryGateway.subscriptionQuery(countCardSummariesQuery,
                 ResponseTypes.instanceOf(Integer.class),
                 ResponseTypes.instanceOf(CountChangedUpdate.class));
         /* When the count changes (new giftcards issued), the UI has to do an entirely new query (this is
@@ -75,25 +92,25 @@ public class CardSummaryDataProvider extends AbstractBackEndDataProvider<CardSum
          * issue event. Therefore, we buffer the updates for 250 milliseconds using reactor, and do the new
          * query at most once per 250ms.
          */
-        countQuery.updates().buffer(Duration.ofMillis(250)).subscribe(
+        countQueryResult.updates().buffer(Duration.ofMillis(250)).subscribe(
                 countChanged -> {
                     log.trace("processing query update for {}: {}", countCardSummariesQuery, countChanged);
                     /* This won't do, would lead to immediate new queries, looping a few times. */
 //                        fireEvent(new DataChangeEvent(this));
                     executorService.execute(() -> fireEvent(new DataChangeEvent(this)));
                 });
-        return countQuery.initialResult().block();
+        return countQueryResult.initialResult().block();
     }
 
     @Synchronized
     public void shutDown() {
-        if (fetchQuery != null) {
-            fetchQuery.cancel();
-            fetchQuery = null;
+        if (fetchQueryResult != null) {
+            fetchQueryResult.cancel();
+            fetchQueryResult = null;
         }
-        if (countQuery != null) {
-            countQuery.cancel();
-            countQuery = null;
+        if (countQueryResult != null) {
+            countQueryResult.cancel();
+            countQueryResult = null;
         }
     }
 
--- src/main/java/com/example/giftcard/query/CardSummaryProjection.java
+++ src/main/java/com/example/giftcard/query/CardSummaryProjection.java
@@ -41,10 +41,21 @@ public class CardSummaryProjection {
     @EventHandler
     public void on(IssuedEvent event) {
         log.trace("projecting {}", event);
+        /*
+         * First, update our read model by inserting the new card. This is done so that upcoming regular
+         * (non-subscription) queries get correct data.
+         */
         newTransactionTemplate.execute(status -> {
             entityManager.persist(new CardSummary(event.getId(), event.getAmount(), event.getAmount()));
             return null;
         });
+        /*
+         * Now, let's serve the subscribed queries by emitting an update. This reads as follows:
+         * - to all current subscriptions of type CountCardSummariesQuery
+         * - for which is true that the id of the gift card having been issued starts with the idStartWith string
+         *   in the query's filter
+         * - send a message that the count of queries matching this query has been changed.
+         */
         queryUpdateEmitter.emit(CountCardSummariesQuery.class,
                 query -> event.getId().startsWith(StringUtils.defaultString(query.getFilter().getIdStartsWith())),
                 new CountChangedUpdate());
@@ -53,11 +64,22 @@ public class CardSummaryProjection {
     @EventHandler
     public void on(RedeemedEvent event) {
         log.trace("projecting {}", event);
+        /*
+         * First, update our read model by updating the existing card. This is done so that upcoming regular
+         * (non-subscription) queries get correct data.
+         */
         CardSummary update = newTransactionTemplate.execute(status -> {
             CardSummary summary = entityManager.find(CardSummary.class, event.getId());
             summary.setRemainingValue(summary.getRemainingValue().subtract(event.getAmount()));
             return summary;
         });
+        /*
+         * Now, let's serve the subscribed queries by emitting an update. This reads as follows:
+         * - to all current subscriptions of type FetchCardSummariesQuery
+         * - for which is true that the id of the gift card having been redeemed starts with the idStartWith string
+         *   in the query's filter
+         * - send a message containing the new state of this gift card summary
+         */
         queryUpdateEmitter.emit(FetchCardSummariesQuery.class,
                 query -> event.getId().startsWith(StringUtils.defaultString(query.getFilter().getIdStartsWith())),
                 update);
